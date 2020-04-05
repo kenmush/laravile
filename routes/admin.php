dc58f241dec2bb14a5e949d64988f145948d05d6
Kamlesh Paul <kamleshpaul414@gmail.com>
2020-04-05 11:14:41 +0530

    admin route added in seprate file

---
+++ routes/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Admin Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::view('/', 'admin.auth.login')->name('admin.login')->middleware('checkAdmin');
+
+Route::group(['middleware' => ['admin']], function () {
+
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+    Route::resource('users', 'UserController');
+    Route::view('register', 'admin.auth.register')->name('admin.register');
+    Route::view('dashboard/register', 'admin.addUser')->name('dashboard.register');
+    
+});
