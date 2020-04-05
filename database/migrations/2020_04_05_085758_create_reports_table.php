46d2d5bc52a6f8407666ba9656a68a9d0badf572
Kamlesh Paul <kamleshpaul414@gmail.com>
2020-04-05 14:39:23 +0530

    all table added

---
+++ database/migrations/2020_04_05_085758_create_reports_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('style_id');
+            $table->unsignedInteger('metric_id');
+            $table->string('links');
+            $table->string('custom_fields')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
